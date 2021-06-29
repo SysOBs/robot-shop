ff815d3872fe883347142103e786b83bc8bedd63
André Bento <andre.pascoal.bento@gmail.com>
2021-06-29 16:31:18 +0100

    Load gen update: Zipkin spans & HTTP transport
    Signed-off-by: André Bento <andre.pascoal.bento@gmail.com>

--- load-gen/robot-shop.py
+++ load-gen/robot-shop.py
@@ -1,9 +1,13 @@
 import os
 import random
 
+from py_zipkin.zipkin import zipkin_span
+from transport import http_transport
+
 from locust import HttpUser, task, between
 from random import choice
 from random import randint
+
 
 class UserBehavior(HttpUser):
     wait_time = between(2, 10)
@@ -35,63 +39,83 @@ class UserBehavior(HttpUser):
         fake_ip = random.choice(self.fake_ip_addresses)
 
         credentials = {
-                'name': 'user',
-                'password': 'password'
-                }
-        res = self.client.post('/api/user/login', json=credentials, headers={'x-forwarded-for': fake_ip})
+            'name': 'user',
+            'password': 'password'
+        }
+        res = self.client.post(
+            '/api/user/login', json=credentials, headers={'x-forwarded-for': fake_ip})
         print('login {}'.format(res.status_code))
-
 
     @task
     def load(self):
-        fake_ip = random.choice(self.fake_ip_addresses)
+        with zipkin_span(
+                service_name='locust-client',
+                span_name='load',
+                transport_handler=http_transport,
+                port=30494,
+                sample_rate=100,
+        ):
+            fake_ip = random.choice(self.fake_ip_addresses)
 
-        self.client.get('/', headers={'x-forwarded-for': fake_ip})
-        user = self.client.get('/api/user/uniqueid', headers={'x-forwarded-for': fake_ip}).json()
-        uniqueid = user['uuid']
-        print('User {}'.format(uniqueid))
+            self.client.get('/', headers={'x-forwarded-for': fake_ip})
+            user = self.client.get('/api/user/uniqueid',
+                                   headers={'x-forwarded-for': fake_ip}).json()
+            uniqueid = user['uuid']
+            print('User {}'.format(uniqueid))
 
-        self.client.get('/api/catalogue/categories', headers={'x-forwarded-for': fake_ip})
-        # all products in catalogue
-        products = self.client.get('/api/catalogue/products', headers={'x-forwarded-for': fake_ip}).json()
-        for i in range(2):
-            item = None
-            while True:
-                item = choice(products)
-                if item['instock'] != 0:
-                    break
+            self.client.get('/api/catalogue/categories',
+                            headers={'x-forwarded-for': fake_ip})
+            # all products in catalogue
+            products = self.client.get(
+                '/api/catalogue/products', headers={'x-forwarded-for': fake_ip}).json()
+            for i in range(2):
+                item = None
+                while True:
+                    item = choice(products)
+                    if item['instock'] != 0:
+                        break
 
-            # vote for item
-            if randint(1, 10) <= 3:
-                self.client.put('/api/ratings/api/rate/{}/{}'.format(item['sku'], randint(1, 5)), headers={'x-forwarded-for': fake_ip})
+                # vote for item
+                if randint(1, 10) <= 3:
+                    self.client.put('/api/ratings/api/rate/{}/{}'.format(
+                        item['sku'], randint(1, 5)), headers={'x-forwarded-for': fake_ip})
 
-            self.client.get('/api/catalogue/product/{}'.format(item['sku']), headers={'x-forwarded-for': fake_ip})
-            self.client.get('/api/ratings/api/fetch/{}'.format(item['sku']), headers={'x-forwarded-for': fake_ip})
-            self.client.get('/api/cart/add/{}/{}/1'.format(uniqueid, item['sku']), headers={'x-forwarded-for': fake_ip})
+                self.client.get(
+                    '/api/catalogue/product/{}'.format(item['sku']), headers={'x-forwarded-for': fake_ip})
+                self.client.get(
+                    '/api/ratings/api/fetch/{}'.format(item['sku']), headers={'x-forwarded-for': fake_ip})
+                self.client.get('/api/cart/add/{}/{}/1'.format(uniqueid,
+                                item['sku']), headers={'x-forwarded-for': fake_ip})
 
-        cart = self.client.get('/api/cart/cart/{}'.format(uniqueid), headers={'x-forwarded-for': fake_ip}).json()
-        item = choice(cart['items'])
-        self.client.get('/api/cart/update/{}/{}/2'.format(uniqueid, item['sku']), headers={'x-forwarded-for': fake_ip})
+            cart = self.client.get(
+                '/api/cart/cart/{}'.format(uniqueid), headers={'x-forwarded-for': fake_ip}).json()
+            item = choice(cart['items'])
+            self.client.get('/api/cart/update/{}/{}/2'.format(uniqueid,
+                            item['sku']), headers={'x-forwarded-for': fake_ip})
 
-        # country codes
-        code = choice(self.client.get('/api/shipping/codes', headers={'x-forwarded-for': fake_ip}).json())
-        city = choice(self.client.get('/api/shipping/cities/{}'.format(code['code']), headers={'x-forwarded-for': fake_ip}).json())
-        print('code {} city {}'.format(code, city))
-        shipping = self.client.get('/api/shipping/calc/{}'.format(city['uuid']), headers={'x-forwarded-for': fake_ip}).json()
-        shipping['location'] = '{} {}'.format(code['name'], city['name'])
-        print('Shipping {}'.format(shipping))
-        # POST
-        cart = self.client.post('/api/shipping/confirm/{}'.format(uniqueid), json=shipping, headers={'x-forwarded-for': fake_ip}).json()
-        print('Final cart {}'.format(cart))
+            # country codes
+            code = choice(self.client.get('/api/shipping/codes',
+                                          headers={'x-forwarded-for': fake_ip}).json())
+            city = choice(self.client.get('/api/shipping/cities/{}'.format(
+                code['code']), headers={'x-forwarded-for': fake_ip}).json())
+            print('code {} city {}'.format(code, city))
+            shipping = self.client.get(
+                '/api/shipping/calc/{}'.format(city['uuid']), headers={'x-forwarded-for': fake_ip}).json()
+            shipping['location'] = '{} {}'.format(code['name'], city['name'])
+            print('Shipping {}'.format(shipping))
+            # POST
+            cart = self.client.post('/api/shipping/confirm/{}'.format(uniqueid),
+                                    json=shipping, headers={'x-forwarded-for': fake_ip}).json()
+            print('Final cart {}'.format(cart))
 
-        order = self.client.post('/api/payment/pay/{}'.format(uniqueid), json=cart, headers={'x-forwarded-for': fake_ip}).json()
-        print('Order {}'.format(order))
+            order = self.client.post('/api/payment/pay/{}'.format(uniqueid),
+                                     json=cart, headers={'x-forwarded-for': fake_ip}).json()
+            print('Order {}'.format(order))
 
     @task
     def error(self):
         if os.environ.get('ERROR') == '1':
             print('Error request')
             cart = {'total': 0, 'tax': 0}
-            self.client.post('/api/payment/pay/partner-57', json=cart, headers={'x-forwarded-for': fake_ip})
-
-
+            self.client.post('/api/payment/pay/partner-57',
+                             json=cart, headers={'x-forwarded-for': fake_ip})
---
+++ load-gen/transport/http_transport.py
@@ -0,0 +1,17 @@
+import requests
+
+from py_zipkin.transport import BaseTransportHandler
+
+
+class HttpTransport(BaseTransportHandler):
+
+    def get_max_payload_bytes(self):
+        return None
+
+    def send(self, encoded_span):
+        # The collector expects a thrift-encoded list of spans.
+        requests.post(
+            'http://194.210.120.176:30494/api/v1/spans',
+            data=encoded_span,
+            headers={'Content-Type': 'application/x-thrift'},
+        )
